e68188cf938534fc762f5912fda669e19df02802
Leonardo Mosimann Conti <leomconti@gmail.com>
2024-05-19 17:41:11 -0300

    fix

--- drive_rooms/alembic/env.py
+++ drive_rooms/alembic/env.py
@@ -6,9 +6,12 @@ from dotenv import load_dotenv
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-from app import models # noqa
+
 from alembic import context
-from app import database
+from app import (
+    database,
+    models,  # noqa
+)
 
 load_dotenv()
 # this is the Alembic Config object, which provides
